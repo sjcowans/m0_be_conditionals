3db2490ba7d256a048c636f790cb813f1d232157
Sean Cowans <zedesia@yahoo.com>
2023-01-04 14:14:41 -0700

    Add comparisons

--- comparisons.rb
+++ comparisons.rb
@@ -23,25 +23,25 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
 # this should print: true
-
+puts number_teachers < number_students
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
 # this should print: false
-
+puts number_teachers == string_teachers
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
 # this should print: true
-
+puts number_teachers != number_students
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
 # this should print: true
-
+puts number_teachers < number_students
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
 # this should print: false
-
+puts number_students >= 21
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
 # this should print: true
-
+puts number_students <= 20
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
 # this should print: true
-
+puts number_students <= 21
 
 #-------------------
 # PART 2: Articulating what you are doing
@@ -54,21 +54,21 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
-
+#It will print true because it's checking if 4 is less than 9
 books = 3
 puts 4 < books
 # YOU DO: Explain.
-
+#There are 3 books (module) and it will print 4 because there are no more than 4 books
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
-
+#There are 6 friends and 2 siblings (both modules) and it will print true
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
-
+#Tehre are 9 attendees and 8 meals (both modules) and it will print true because they are not equal
 
 #-------------------
 # PART 3: Logical Operators
@@ -90,15 +90,17 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
-
+puts loves_to_play && puppy
 # What did your final line of code evaluate to? Why do you think that is? Explain.
 # ANSWER:
+comparisons.rb:102:in `<main>': undefined local variable or method `puppy' for main:Object (NameError)
+We never defined puppy so the variable is unassigned.
